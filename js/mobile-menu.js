b8d6def04e4a68a4334aec7ae2d9d3a3d100f350
marina-zalozna <106180626+marina-zalozna@users.noreply.github.com>
2022-08-01 19:44:35 +0300

    mob2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,14 @@
 (() => {
-    const refs = {
-      openMobileMenuBtn: document.querySelector("[data-mobile-menu-open]"),
-      closeMobileMenuBtn: document.querySelector("[data-mobile-menu-close]"),
-      mobileMenu: document.querySelector("[data-mobile-menu]"),
-    };
+  const refs = {
+    openMobileMenuBtn: document.querySelector("[data-mobile-menu-open]"),
+    closeMobileMenuBtn: document.querySelector("[data-mobile-menu-close]"),
+    mobileMenu: document.querySelector("[data-mobile-menu]"),
+  };
 
-    refs.openMobileMenuBtn.addEventListener("click", toggleMobileMenu);
-    refs.closeMobileMenuBtn.addEventListener("click", toggleMobileMenu);
+  refs.openMobileMenuBtn.addEventListener("click", toggleMobileMenu);
+  refs.closeMobileMenuBtn.addEventListener("click", toggleMobileMenu);
 
-    function toggleMobileMenu() {
-      refs.mobileMenu.classList.toggle("is-hidden");
-    }
-  })();
+  function toggleMobileMenu() {
+    refs.mobileMenu.classList.toggle("is-open");
+  }
+})();
